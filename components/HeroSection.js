b0c4ab58028696b687f48bf8f3536bee95ef383f
sitiamalianurfaidah <150977479+sitiamalianurfaidah@users.noreply.github.com>
2025-07-20 00:39:37 +0700

    Hero Section

---
+++ components/HeroSection.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const HeroSection = () => {
+    return (
+        <section
+        id="hero"
+        className="relative w-full h-[100vh] flex items-center justify-center text-white overflow-hidden"
+        >
+        <Image
+            src="/hero.jpg" 
+            alt="Hero Background"
+            fill
+            priority
+            className="absolute w-full h-full object-cover object-center"
+        />
+
+        {/* Overlay konten */}
+        <div className="hero-overlay text-white text-center px-4">
+            <h1 className="text-6xl md:text-7xl font-extrabold mb-6 leading-tight animate-title">
+            <span className="inline-block mr-2 text-white-300 animate-word">Cerita</span>
+            <span className="inline-block text-white-300 animate-word delay-200">Films</span>
+            </h1>
+            <p className="text-lg md:text-xl mb-6 text-gray-100 max-w-2xl mx-auto">
+            Saksikan <span className="font-bold">SORE: Istri dari Masa Depan</span>, film terbaru dari Cerita Films. 
+            Penuh makna, cinta, dan waktu yang tak terduga.
+            </p>
+            <a
+                href="https://www.tix.id/movie/sore-istri-dari-masa-depan/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-block bg-orange-300 hover:bg-orange-500 text-white px-6 py-3 rounded-md transition"
+                >
+                Tonton Sekarang
+            </a>
+
+        </div>
+        </section>
+    );
+};
+
+export default HeroSection;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import movies from "@/data/movies";
 import Filter from "@/components/Filter";
 import MovieList from "@/components/MovieList";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   const [selectedTag, setSelectedTag] = useState(null);
@@ -13,14 +14,17 @@ export default function Home() {
     : movies;
 
   return (
-    <main className="min-h-screen p-6 bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">Cerita Films</h1>
-      <Filter
-        allTags={allTags}
-        selectedTag={selectedTag}
-        setSelectedTag={setSelectedTag}
-      />
-      <MovieList movies={filteredMovies} />
+    <main className="min-h-screen bg-gray-100">
+      <HeroSection />      
+      <section id="movielist" className="p-6">
+        <h2 className="text-3xl font-bold mb-6">Cerita Films</h2>
+        <Filter
+          allTags={allTags}
+          selectedTag={selectedTag}
+          setSelectedTag={setSelectedTag}
+        />
+        <MovieList movies={filteredMovies} />
+      </section>
     </main>
   );
 }
